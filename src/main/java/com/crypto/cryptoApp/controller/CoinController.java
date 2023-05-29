cf99ae7d78826c2ab58bc2d0f3ce668634ecf374
giana medeiros <gianamedeiros.00510@gmail.com>
2023-05-29 16:32:41 -0300

    Using bean to initialize wallet data

--- src/main/java/com/crypto/cryptoApp/controller/CoinController.java
+++ src/main/java/com/crypto/cryptoApp/controller/CoinController.java
@@ -3,10 +3,12 @@ package com.crypto.cryptoApp.controller;
 import com.crypto.cryptoApp.entity.Coin;
 import com.crypto.cryptoApp.repository.CoinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -16,10 +18,39 @@ public class CoinController {
     @Autowired
     private CoinRepository repository;
 
+    @Bean
+    public  Coin init(){
+        Coin c1 = new Coin();
+        c1.setName("BITCOIN");
+        c1.setPrice(new BigDecimal(150));
+        c1.setQuantity(new BigDecimal(0.0025));
+        c1.setDateTime(new Timestamp(System.currentTimeMillis()));
+
+        Coin c2 = new Coin();
+        c2.setName("BITCOIN");
+        c2.setPrice(new BigDecimal(100));
+        c2.setQuantity(new BigDecimal(0.0030));
+        c2.setDateTime(new Timestamp(System.currentTimeMillis()));
+
+        Coin c3 = new Coin();
+        c3.setName("ETHEREUM");
+        c3.setPrice(new BigDecimal(200));
+        c3.setQuantity(new BigDecimal(0.0020));
+        c3.setDateTime(new Timestamp(System.currentTimeMillis()));
+
+        repository.insert(c1);
+        repository.insert(c2);
+        repository.insert(c3);
+
+        return c1;
+    }
+
+
     @GetMapping()
     public ResponseEntity<List> listAll(){
 
         return  new ResponseEntity<>(repository.listAll(),HttpStatus.OK);
+
     }
 
     @GetMapping("/{name}")
